b6821e8ad8bd34fa6de1796690512f60319f96e0
lotus <daniel.younghwan.lee@gmail.com>
2022-09-06 16:35:51 -0400

    figured out how to import image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import hikingImage from "../../assets/hiking.jpg";
 
 function About() {
     return (
         <section className="container is-flex is-flex-direction-column">
             <h1 className="title is-size-2 mt-6 mb-5 has-text-centered">About Me</h1>
-            <p className="is-size-4 is-size-5-mobile has-text-centered-mobile">
-                Korean-American born and raised in New York.
-                A practitioner of street-dance, a big sports fan, and a seasoned traveler.
-                Once I began learning how to code and realized how natural it came to me, I never turned back.
-                My experience with the Columbia Engineering Coding Bootcamp has been an enjoyable one, where every
-                piece of new knowledge brings the excitement of new possibilities.
-                There, I have established my skills in front-end and back-end technologies, while honing my ability
-                to collaborate with others.
-                I look forward to the next part of this journey: joining a professional team with whom my work is
-                meaningful and I can continue to grow.
-            </p>
+            <div className="columns">
+                <div className="column is-3 is-8-mobile is-offset-2-mobile">
+                    <img src={hikingImage} alt="hiking"></img>
+                </div>
+                <div className="column is-9 is-10-mobile is-offset-1-mobile">
+                    <p className="is-size-4 is-size-5-mobile has-text-centered-mobile">
+                        Korean-American born and raised in New York.
+                        A practitioner of street-dance, a big sports fan, and a seasoned traveler.
+                        Once I began learning how to code and realized how natural it came to me, I never turned back.
+                        My experience with the Columbia Engineering Coding Bootcamp has been an enjoyable one, where every
+                        piece of new knowledge brings the excitement of new possibilities.
+                        There, I have established my skills in front-end and back-end technologies, while honing my ability
+                        to collaborate with others.
+                        I look forward to the next part of this journey: joining a professional team with whom my work is
+                        meaningful and I can continue to grow.
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,14 +4,18 @@ function Nav() {
 
     return (
         <section className="hero is-medium is-black">
+            <div className="hero-head">
             <nav className="navbar">
                 <h1 id="navbar-name" className="navbar-brand has-text-white is-size-1 my-4 mx-6 has-text-centered"
                     >Daniel Y. Lee</h1>
                 <div id="navbar-sections" className="navbar-end px-6 is-flex is-flex-direction-row">
-                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#about-me">About Me</a>
+                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#about-me">About</a>
                     <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#projects">Projects</a>
+                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#contact">Contact</a>
+                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#resume">Resume</a>
                 </div>
             </nav>
+            </div>
         </section>
     );
 }
