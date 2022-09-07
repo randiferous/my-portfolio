f9975574b2ad404aa133f9ef249b8526e94ca68a
lotus <daniel.younghwan.lee@gmail.com>
2022-09-07 00:22:56 -0400

    figured out how to separate components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from './components/Nav';
 import About from './components/About';
 import Project from './components/Project';
@@ -7,13 +8,19 @@ import Project from './components/Project';
 
 function App() {
   return (
+    <Router>
     <div>
       <Nav></Nav>
       <main>
-        <About></About>
-        <Project></Project>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about-me" element={<About />} />
+          <Route path="/projects" element={<Project />} />
+        </Routes>
       </main>
     </div>
+    </Router>
+
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
 
@@ -6,11 +7,11 @@ function Nav() {
         <section className="hero is-medium is-black">
             <div className="hero-head">
             <nav className="navbar">
-                <a id="navbar-name" className="navbar-brand has-text-white is-size-1 my-4 mx-6 has-text-centered"
-                    href="/">Daniel Y. Lee</a>
+                <Link id="navbar-name" className="navbar-brand has-text-white is-size-1 my-4 mx-6 has-text-centered"
+                    to="/">Daniel Y. Lee</Link>
                 <div id="navbar-sections" className="navbar-end px-6 is-flex is-flex-direction-row">
-                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#about-me">About</a>
-                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#projects">Projects</a>
+                    <Link className="navbar-item has-text-white is-size-3 is-size-4-mobile" to="/about-me">About</Link>
+                    <Link className="navbar-item has-text-white is-size-3 is-size-4-mobile" to="/projects">Projects</Link>
                     <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#contact">Contact</a>
                     <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" href="#resume">Resume</a>
                 </div>
