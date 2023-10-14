4439908cec65eeb856a0986fa3fa366f95ed534c
Daniel Y. Lee <daniel.lee@invene.com>
2023-10-14 00:45:03 -0400

    testing first push text change

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -23,13 +23,6 @@ function About() {
                         <p className="is-size-4 is-size-5-mobile has-text-centered has-text-centered-mobile">
                             Korean-American born and raised in New York.
                             A practitioner of street-dance, a big sports fan, and a seasoned traveler.
-                            Once I began learning how to code and realized how natural it came to me, I never turned back.
-                            My experience with the Columbia Engineering Coding Bootcamp has been an enjoyable one, where every
-                            piece of new knowledge brings the excitement of new possibilities.
-                            There, I have established my skills in front-end and back-end technologies, while honing my ability
-                            to collaborate with others.
-                            I look forward to the next part of this journey: joining a professional team with whom my work is
-                            meaningful and I can continue to grow.
                         </p>
                         <div
                             className="is-flex flex-direction-row is-flex-wrap-wrap is-justify-content-space-around columns mt-1 wow">
