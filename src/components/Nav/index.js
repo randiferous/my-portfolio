ce16a0f740aa2af7909f3e2336b2b24f5481fba5
Daniel Y. Lee <128609776+danielyoung1lee@users.noreply.github.com>
2023-11-03 12:59:43 -0400

    latest november updates

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 function Nav(props) {
-
     return (
-            <nav className="navbar is-medium">
+            <nav className="navbar is-medium padding-t-1px">
                 <a id="navbar-name" className="navbar-brand has-text-white is-size-1 my-4 mx-6 has-text-centered" onClick={() => props.changeCurrentPage("About")}
                     href="#my-portfolio">Daniel Y. Lee</a>
                 <div id="navbar-sections" className="navbar-end px-6 is-flex is-flex-direction-row">
-                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" onClick={() => props.changeCurrentPage("About")} href="#about">About</a>
-                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" onClick={() => props.changeCurrentPage("Projects")} href="#projects">Portfolio</a>
-                    <a className="navbar-item has-text-white is-size-3 is-size-4-mobile" onClick={() => props.changeCurrentPage("Contact")} href="#contact">Contact</a>
+                    <a className={`navbar-item has-text-white is-size-3 is-size-4-mobile ${props.currentPage === "About" ? "is-underlined" : ""}`} 
+                        onClick={() => props.changeCurrentPage("About")} href="#about">About</a>
+                    <a className={`navbar-item has-text-white is-size-3 is-size-4-mobile ${props.currentPage === "Projects" ? "is-underlined" : ""}`} 
+                        onClick={() => props.changeCurrentPage("Projects")} href="#projects">Work</a>
+                    <a className={`navbar-item has-text-white is-size-3 is-size-4-mobile ${props.currentPage === "Contact" ? "is-underlined" : ""}`} 
+                        onClick={() => props.changeCurrentPage("Contact")} href="#contact">Contact</a>
                 </div>
             </nav>
     );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,58 +1,51 @@
 import React from 'react';
 import covidTracker from "../../assets/covid-tracker.PNG";
 import restaurateur from "../../assets/restaurateur.PNG";
-import shopshop from "../../assets/shopshop-screenshot.PNG";
-import techMatchup from "../../assets/tech-matchup-screenshot.PNG";
-import healthcareHero from "../../assets/healthcare-hero.PNG"
+import invenelogo from "../../assets/invene_logo.png"
+import edx_2u from "../../assets/2u_edx.jpg"
+import nhlApp from "../../assets/nhl_app_screenshot.png"
 
 function Project() {
 
     return (
         <section className="container pb-5">
-            <h1 id="projects" className="title is-size-1 my-6 pb-3 has-text-centered">Projects</h1>
+            <h1 id="projects" className="title is-size-1 my-6 pb-3 has-text-centered">Projects & Work</h1>
 
             <div className="tile is-ancestor animate__animated animate__fadeInDown">
                 <div className="tile is-6 is-parent">
                     <div className="tile is-child box">
-                        <a href="https://intense-inlet-07195.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <a href="https://invene.com/" target="_blank" rel="noreferrer" className="is-flex is-justify-content-center margin-lr-15">
                             <figure className="image">
-                                <img src={restaurateur} alt="restaurateur" />
+                                <img src={invenelogo} alt="invene logo" />
                             </figure>
                         </a>
-                        <h2 className="title has-text-centered my-2">Restaurateur</h2>
-                        <p className="is-size-5 has-text-weight-semibold has-text-centered">Team Role: Project Manager</p>
-                        <p className="is-size-5 my-1 has-text-centered">MERN-stack single-page application that provides
-                            resources and instructions for aspiring entrepreneurs who want to open a new restaurant business.</p>
-                        <p className="is-italic has-text-weight-semibold my-1 has-text-centered is-size-6 my-1">Featured:
-                            React, GraphQL, Apollo, Node.js, MongoDB, Mongoose, JWT, Bootstrap, SPA</p>
-                        <figure className="image is-48x48 mx-auto my-2">
-                            <a href="https://github.com/Muzan67/restaurateur.git" target="_blank" rel="noreferrer">
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                                    alt="Github icon" />
-                            </a>
-                        </figure>
+                        <h2 className="title has-text-centered my-2">Invene</h2>
+                        <p className="is-size-5 has-text-weight-semibold has-text-centered">Software Engineer, Jr.</p>
+                        <p className="is-size-5 my-1 has-text-centered">Contributing to products that make the world a better place.
+                            Growing daily both in technical skills and professionalism.</p>
+                        <p className="is-italic has-text-weight-semibold has-text-centered is-size-6 my-3">Nov 2022 - Current</p>
                     </div>
                 </div>
-
                 <div className="tile is-6 is-parent">
                     <div className="tile is-child box">
-                        <a href="https://radiant-reaches-92570.herokuapp.com/" target="_blank" rel="noreferrer">
-                            <figure className="image">
-                                <img src={healthcareHero} alt="Huxley the HealthCare Hero" />
+                        <a href="https://main.d35ntjugpu1957.amplifyapp.com/teams" target="_blank" rel="noreferrer">
+                            <figure className="image mt-6">
+                                <img src={nhlApp} alt="NHL app screenshot" />
                             </figure>
                         </a>
-                        <h2 className="title has-text-centered my-2">Healthcare Hero</h2>
-                        <p className="is-size-5 has-text-weight-semibold has-text-centered">Team Role: Lead Developer</p>
-                        <p className="is-size-5 my-1 has-text-centered">Interactive full-stack application that allows
-                            healthcare providers to share their service information on a public forum.</p>
-                        <p className="is-italic has-text-weight-semibold my-1 has-text-centered is-size-6 my-1">Featured:
-                            Node.js, Express, MySQL, Sequelize, MVC, Authentication, Handlebars</p>
-                        <figure className="image is-48x48 mx-auto my-2">
-                            <a href="https://github.com/randiferous/healthcare-hero.git" target="_blank" rel="noreferrer">
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                                    alt="GitHub icon" />
-                            </a>
-                        </figure>
+                        <h2 className="title has-text-centered my-2 mt-5">My NHL App</h2>
+                        <p className="is-size-5 has-text-weight-semibold has-text-centered">Individual Developer</p>
+                        <p className="is-size-5 my-1 has-text-centered">React application using AWS services for deployment, authentication, and storage, 
+                        that provides real-time information about the National Hockey League.</p>
+                        <div className="is-flex is-flex-direction-row is-justify-content-center is-align-items-center">
+                            <p className="is-italic has-text-weight-semibold px-3 my-1 has-text-centered is-size-6">Oct 2023</p>
+                            <figure className="image is-48x48 my-2">
+                                <a href="https://github.com/randiferous/stats-app" target="_blank" rel="noreferrer">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                        alt="Github icon" />
+                                </a>
+                            </figure>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,67 +53,65 @@ function Project() {
             <div className="tile is-ancestor mb-6 animate__animated animate__fadeInDown">
                 <div className="tile is-5 is-parent">
                     <div className="tile is-child box">
-                        <a href="https://randiferous.github.io/group-covid-project/" target="_blank" rel="noreferrer">
+                        <a href="https://intense-ravine-89236.herokuapp.com/" target="_blank" rel="noreferrer">
                             <figure className="image">
-                                <img src={covidTracker} alt="Group Project: COVID-19 tracker" />
+                                <img src={restaurateur} alt="restaurateur" />
                             </figure>
                         </a>
-                        <h2 className="title has-text-centered my-2 ">COVID-19 Tracker</h2>
-                        <p className="is-size-5 has-text-weight-semibold has-text-centered">Team Role: Project Manager</p>
-                        <p className="is-size-5 my-1 has-text-centered">Front-end application that provides up-to-date COVID-19
-                            data as well as general information on a country basis.</p>
-                        <p className="is-italic has-text-weight-semibold my-1 has-text-centered is-size-6 my-1">Featured:
-                            HTML, CSS, JavaScript, Responsive Design, GitHub, RESTful APIs</p>
-                        <figure className="image is-48x48 mx-auto my-2">
-                            <a href="https://github.com/randiferous/group-covid-project" target="_blank" rel="noreferrer">
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                                    alt="Group COVID Project GitHub" />
-                            </a>
-                        </figure>
+                        <h2 className="title has-text-centered my-2 ">Restaurateur</h2>
+                        <p className="is-size-5 has-text-weight-semibold has-text-centered">Lead Developer</p>
+                        <p className="is-size-5 my-1 has-text-centered">MERN-stack single-page application that provides
+                            resources and instructions for aspiring entrepreneurs who want to open a new restaurant business.</p>
+                        <div className="is-flex is-flex-direction-row is-justify-content-center is-align-items-center">
+                            <p className="is-italic has-text-weight-semibold px-3 my-1 has-text-centered is-size-6">May 2022</p>
+                            <figure className="image is-48x48 my-2">
+                                <a href="https://github.com/randiferous/restaurateur" target="_blank" rel="noreferrer">
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                        alt="Github icon" />
+                                </a>
+                            </figure>
+                        </div>
                     </div>
                 </div>
-
                 <div className="tile is-7 is-vertical is-parent">
                     <div className="tile is-child box columns">
                         <div className="column is-6 my-auto">
-                            <a href="https://peaceful-springs-45091.herokuapp.com/" target="_blank" rel="noreferrer">
+                            <a href="https://2u.com/" target="_blank" rel="noreferrer">
                                 <figure className="image" id="shop-shop">
-                                    <img src={shopshop} alt="Shop-Shop" />
+                                    <img src={edx_2u} alt="2U edx" />
                                 </figure>
                             </a>
                         </div>
                         <div className="column is-6">
-                            <h3 className="has-text-centered is-size-3">Shop-Shop</h3>
-                            <p className="has-text-centered is-size-6">E-commerce application built with the MERN-stack that allows authenticated users to add products to a shopping cart and purchase them.</p>
-                            <p className="is-italic has-text-weight-semibold has-text-centered is-size-6 my-1">Featured:
-                                React, MongoDB, GraphQL, Stripe, JWT, Node.js</p>
-                            <figure className="image is-48x48 mx-auto my-2">
-                                <a href="https://github.com/randiferous/shop-shop.git" target="_blank" rel="noreferrer">
-                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                                        alt="GitHub icon" />
-                                </a>
-                            </figure>
+                            <h3 className="has-text-centered is-size-3">2U</h3>
+                            <p className="is-size-5 has-text-weight-semibold has-text-centered">TA, Web Development</p>
+                            <p className="has-text-centered is-size-6">Teaching Assistant for UK Skills for Life and edX Full-Stack Coding bootcamps.
+                            </p>
+                            <p className="is-italic has-text-weight-semibold has-text-centered is-size-6 my-3">Sept 2022 - July 2023</p>
                         </div>
                     </div>
                     <div className="tile is-child box columns">
                         <div className="column is-6 my-auto">
-                            <a href="https://infinite-caverns-98317.herokuapp.com/" target="_blank" rel="noreferrer">
+                            <a href="https://randiferous.github.io/group-covid-project/" target="_blank" rel="noreferrer">
                                 <figure className="image">
-                                    <img src={techMatchup} alt="Tech-Matchup" />
+                                    <img src={covidTracker} alt="Group Project: COVID-19 tracker" />
                                 </figure>
                             </a>
                         </div>
                         <div className="column is-6">
-                            <h3 className="has-text-centered is-size-3">Tech Matchup</h3>
-                            <p className="has-text-centered is-size-6">MERN-stack application that allows boot camp students to create a matchup between two technologies and vote on their favorite technology.</p>
-                            <p className="is-italic has-text-weight-semibold has-text-centered is-size-6 my-1">Featured:
-                                React, GraphQL, Node.js, MongoDB</p>
-                            <figure className="image is-48x48 mx-auto my-2">
-                                <a href="https://github.com/randiferous/tech-matchup.git" target="_blank" rel="noreferrer">
-                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                                        alt="GitHub icon" />
-                                </a>
-                            </figure>
+                            <h3 className="has-text-centered is-size-3">COVID-19 Tracker</h3>
+                            <p className="is-size-5 has-text-weight-semibold has-text-centered">Team Lead</p>
+                            <p className="has-text-centered is-size-6">Front-end application that provides up-to-date COVID-19
+                            data as well as general information on a country basis.  My first group project ever!</p>
+                            <div className="is-flex is-flex-direction-row is-justify-content-center is-align-items-center">
+                                <p className="is-italic has-text-weight-semibold has-text-centered is-size-6 px-3 my-1">May 2022</p>
+                                <figure className="image is-48x48 my-2">
+                                    <a href="https://github.com/randiferous/group-covid-project" target="_blank" rel="noreferrer">
+                                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                            alt="Group COVID Project GitHub" />
+                                    </a>
+                                </figure>
+                            </div>
                         </div>
                     </div>
                 </div>
